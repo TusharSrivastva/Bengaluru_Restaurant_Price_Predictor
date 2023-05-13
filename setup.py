7a13b18f4234fa9f52332aced41a48185d7e9f1b
TusharSrivastva <tusharsrivastva1@gmail.com>
2023-05-13 18:08:12 +0530

    Added steup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E = '-e .'
+def get_requirements(file: str)->List[str]:
+    '''
+    This function reads from requirements file and return a list of 
+    requirements
+    '''
+
+    with open(file, mode='r') as f:
+        requirements = f.readlines()
+    
+    requirements = [i.replace('\n','') for i in requirements]   # Remove '\n' from the end of each line
+
+    if HYPHEN_E in requirements:
+        requirements.remove(HYPHEN_E)
+
+    return requirements
+
+
+setup(
+    name = 'Bengaluru Restaurant Price Predictor',
+    version = '0.0.1',
+    description = 'Given some input data(rating, online delivery, booking, etc) this package predicts restraunt cost for two people in Bengaluru',
+    author = 'Tushar Srivastava',
+    author_email = 'tusharsrivastva1@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    )
