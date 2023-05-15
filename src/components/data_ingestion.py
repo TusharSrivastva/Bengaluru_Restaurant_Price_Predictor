252962fcc2bb97ee8898a406e1864ac1ca2c9fbe
TusharSrivastva <tusharsrivastva1@gmail.com>
2023-05-15 17:23:06 +0530

    Added documentations

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,29 +1,63 @@
 import os 
 import sys
-from src.exception import CustomException
-from src.logger import logging
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
 
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
+
+from typing import Type, Tuple
+
 @dataclass
 class DataIngestionConfig:
+    """
+    Class DataIngestionConfig initializes the path of train data, test data and original dataset.
+
+        * train_data_path: Path of train dataset
+        * test_data_path: Path of test dataset
+        * raw_data_path: Path of original dataset
+
+    """
     # Initializing paths for datasets
-    train_data_path = os.path.join('artifacts', 'train.csv')
-    test_data_path = os.path.join('artifacts', 'test.csv')
-    raw_data_path = os.path.join('artifacts', 'data.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
 
 class DataIngestion:
-    def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+    """
+    class DataIngestion
+        * __init__() -> None
+        * intiate_data_ingestion() -> Tuple[str, str]
+
+        This class is used for implementing data ingestion, it inherits from class DataIngestionConfig.
+        The intiate_data_ingestion() imports the CSV dataset from its locations using pandas library, 
+        splits it into train and test datasets and store them in artifacts folder.
+
+    """
+    def __init__(self) -> None:
+        self.ingestion_config: Type[DataIngestionConfig] = DataIngestionConfig()
     
-    def intiate_data_ingestion(self)->str:
+    def intiate_data_ingestion(self) -> Tuple[str, str]:
+        """
+        This function implements data ingestion. The CSV dataset is import using pandas library. The 
+        dataset is then split into train and test data, further the raw, train and test datasets are 
+        stored in the artifacts folder and file paths to train and test datasets are returned as 
+        outputs.
+
+        * df: Original dataset
+        * train: Training dataset
+        * test: Test dataset
+
+        """
         logging.info("Data ingestion has begun")
         try:
             # Importing dataset using pandas
-            df = pd.read_csv('notebook\data\zomato.csv')
+            df: pd.DataFrame = pd.read_csv('notebook\data\zomato.csv')
             logging.info("Dataset imported")
 
             # Creating directory
@@ -31,9 +65,10 @@ class DataIngestion:
 
             # Splitting dataset into test and train
             logging.info("train_test_split initiated")
+            train: pd.DataFrame; test: pd.DataFrame
             train, test = train_test_split(df, test_size=0.2, random_state=9)
 
-            # Saving datasets as csv
+            # Saving raw, train and test datasets as csv
             df.to_csv(self.ingestion_config.raw_data_path, index = False)
             logging.info("Raw data saved as csv")
             
@@ -54,6 +89,9 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 if __name__=="__main__":
-    obj = DataIngestion()
-    train_data_path, test_data_path = obj.intiate_data_ingestion()
+    # Data Ingestion
+    train_data_path, test_data_path = DataIngestion().intiate_data_ingestion()
+
+    # Data Transformation
+    X_train, y_train, X_test, y_test, preprocessor_path = DataTransformation().initiate_data_transformation(train_data_path, test_data_path)
 
